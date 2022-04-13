b40f2339a274f52d06aff4d99360c1438e3d7c0c
Yaremenko Roman <roman.jaremenko@gmail.com>
2022-04-13 14:56:01 +0300

    testSwipeArticle() added

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,10 +1,12 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -105,7 +107,9 @@ public class FirstTest {
             By.id("org.wikipedia:id/view_page_title_text"),
             "Cannot find article title",
             30);
+
     String article_title = title_element.getAttribute("text");
+
     Assert.assertEquals(
             "Unexpected title",
             "Java (programming language)",
@@ -173,6 +177,36 @@ public class FirstTest {
             "Search results do not contain expected values");
   }
 
+  @Test
+  public void testSwipeArticle() {
+    waitForElementAndClick(
+            By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+            "Cannot find 'Search Wikipedia' Input",
+            15);
+
+    waitForElementAndSendKeys(
+            By.xpath("//*[contains(@text,'Search')]"),
+            "Java",
+            "Cannot find search input",
+            15);
+
+    waitForElementAndClick(
+            By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+            "Cannot find 'Search Wikipedia' Input",
+            15);
+
+    waitForElementPresent(
+            By.id("org.wikipedia:id/view_page_title_text"),
+            "Cannot find article title",
+            30);
+
+    swipeUp(2000);
+    swipeUp(2000);
+    swipeUp(2000);
+    swipeUp(2000);
+    swipeUp(2000);
+  }
+
   private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
     WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
     wait.withMessage(error_message + "\n");
@@ -237,5 +271,14 @@ public class FirstTest {
       String exp_text = expected_text;
       Assert.assertTrue(error_message, act_text.toLowerCase().contains(exp_text.toLowerCase()));
     }
+  }
+
+  private void swipeUp(int timeOfSwipe) {
+    TouchAction action = new TouchAction(driver);
+    Dimension size = driver.manage().window().getSize();
+    int x = size.width / 2;
+    int start_y = (int) (size.height * 0.8);
+    int end_y = (int) (size.height * 0.2);
+    action.press(x, start_y).waitAction(timeOfSwipe).moveTo(x, end_y).release().perform();
   }
 }
