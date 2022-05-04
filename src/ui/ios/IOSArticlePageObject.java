63d8b43b48e33649624880cde1d90668b606e603
Yaremenko Roman <roman.jaremenko@gmail.com>
2022-05-04 23:24:55 +0300

    class IOSArticlePageObject added

---
+++ src/ui/ios/IOSArticlePageObject.java
@@ -0,0 +1,18 @@
+package ui.ios;
+
+import io.appium.java_client.AppiumDriver;
+import ui.ArticlePageObject;
+
+public class IOSArticlePageObject extends ArticlePageObject {
+
+  static {
+    TITLE = "id:Java (programming language)";
+    FOOTER_ELEMENT = "id:View article in browser";
+    OPTIONS_ADD_TO_MY_LIST_BUTTON = "id:Save for later";
+    CLOSE_ARTICLE_BUTTON = "id:Back";
+  }
+
+  public IOSArticlePageObject(AppiumDriver driver) {
+    super(driver);
+  }
+}
