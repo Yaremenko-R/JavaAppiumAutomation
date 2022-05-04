361077906195677636b8d36e11c9f387ad9494d0
Yaremenko Roman <roman.jaremenko@gmail.com>
2022-05-04 18:03:16 +0300

    class SearchPageObjectFactory added

---
+++ src/ui/factories/SearchPageObjectFactory.java
@@ -0,0 +1,10 @@
+package ui.factories;
+
+import io.appium.java_client.AppiumDriver;
+import ui.SearchPageObject;
+
+public class SearchPageObjectFactory {
+  public static SearchPageObject get(AppiumDriver driver) {
+
+  }
+}
