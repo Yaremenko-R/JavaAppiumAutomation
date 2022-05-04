95bcfbb4087beedbda96318a275350738f3d7bc5
Yaremenko Roman <roman.jaremenko@gmail.com>
2022-05-04 18:04:50 +0300

    SearchPageObject refactoring of constants

--- src/ui/SearchPageObject.java
+++ src/ui/SearchPageObject.java
@@ -2,16 +2,16 @@ package ui;
 
 import io.appium.java_client.AppiumDriver;
 
-public class SearchPageObject extends MainPageObject {
-  private static final String
-          SEARCH_INIT_ELEMENT = "xpath://*[contains(@text,'Search Wikipedia')]",
-          SEARCH_INPUT = "xpath://*[contains(@text,'Search')]",
-          SEARCH_CANCEL_BUTTON = "id:org.wikipedia:id/search_close_btn",
-          SEARCH_RESULT_BY_SUBSTRING_TPL = "xpath://*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='{SUBSTRING}']",
-          SEARCH_RESULT_ELEMENT = "xpath://*[@resource-id='org.wikipedia:id/search_results_list']/*[@resource-id='org.wikipedia:id/page_list_item_container']",
-          SEARCH_EMPTY_RESULT_ELEMENT = "xpath://*[@text='No results found']",
-          SEARCH_ARTICLE_TITLE = "id:org.wikipedia:id/page_list_item_title",
-          SEARCH_RESULT_BY_SUBSTRING_TITLE_DESC_TPL = "xpath://*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@resource-id='org.wikipedia:id/page_list_item_title/@text='{TITLE}' and @resource-id='org.wikipedia:id/page_list_item_description/@text='{DESC}']";
+abstract public class SearchPageObject extends MainPageObject {
+   protected static String
+          SEARCH_INIT_ELEMENT,
+          SEARCH_INPUT,
+          SEARCH_CANCEL_BUTTON,
+          SEARCH_RESULT_BY_SUBSTRING_TPL,
+          SEARCH_RESULT_ELEMENT,
+          SEARCH_EMPTY_RESULT_ELEMENT,
+          SEARCH_ARTICLE_TITLE,
+          SEARCH_RESULT_BY_SUBSTRING_TITLE_DESC_TPL;
 
   public SearchPageObject(AppiumDriver driver) {
     super(driver);
