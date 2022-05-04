378616c92b23ca2b97f0709b87f0ad5a7121b32c
Yaremenko Roman <roman.jaremenko@gmail.com>
2022-05-04 18:11:13 +0300

    class SearchPageObjectFactory done

--- src/ui/factories/SearchPageObjectFactory.java
+++ src/ui/factories/SearchPageObjectFactory.java
@@ -1,10 +1,17 @@
 package ui.factories;
 
 import io.appium.java_client.AppiumDriver;
+import lib.Platform;
 import ui.SearchPageObject;
+import ui.android.AndroidSearchPageObject;
+import ui.ios.IOSSearchPageObject;
 
 public class SearchPageObjectFactory {
   public static SearchPageObject get(AppiumDriver driver) {
-
+    if (Platform.getInstance().isAndroid()) {
+      return new AndroidSearchPageObject(driver);
+    } else {
+      return new IOSSearchPageObject(driver);
+    }
   }
 }
