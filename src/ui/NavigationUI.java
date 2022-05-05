852df37bfb3b08a1c8e6124b96083bc55ca7b287
Yaremenko Roman <roman.jaremenko@gmail.com>
2022-05-05 15:01:42 +0300

    class NavigationUI refactoring of constants

--- src/ui/NavigationUI.java
+++ src/ui/NavigationUI.java
@@ -2,9 +2,9 @@ package ui;
 
 import io.appium.java_client.AppiumDriver;
 
-public class NavigationUI extends MainPageObject {
-  private static final String
-          MY_LISTS_LINK = "xpath://android.widget.FrameLayout[@content_desc='My lists']";
+abstract public class NavigationUI extends MainPageObject {
+  protected static String
+          MY_LISTS_LINK;
 
   public NavigationUI(AppiumDriver driver) {
     super(driver);
